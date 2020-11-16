981ba236e326e168d4e7d59838bc00fa652c99d5
anilshetty11408 <anilshetty0707@gmail.com>
2020-11-16 22:45:08 +0530

    first commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringBootRestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestserviceApplication.class, args);
+	}
+
+}
